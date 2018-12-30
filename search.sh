4a92b3f7ab8109f85fcd9c1ffcca5251fc4f9bca
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2018-12-30 13:37:03 -0200

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-DIR=/tmp
-echo -n "[+] Running Wget in $1"
-wget -U " Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko" -q $1 -O $DIR/$1.html 
-echo " ->>> DONE"
-echo "[+] Starting routine to find hosts from domain"
-for i in $(egrep -i '<a href="\w+:..\w+.\w+.\w+' $DIR/$1.html  -o | cut -d '/' -f 3 | sort  | uniq | grep $1);do
-#	host -4 $i
-	echo "$i : " $(dig $i +short)
+function fail(){
+  echo "[*] Nenhuma NS Encontrado para o dominio $DOMAIN, encerrando..."
+  exit 1
+}
+DOMAIN=$1
+echo "[+] Iniciando Procedimentos. Alvo -> $DOMAIN"
+NS=$(dig -t ns $DOMAIN +short)
+[ -n "$NS" ] || fail 
+echo "[+] Encontrado $(echo $NS | tr ' ' '\n' | wc -l ) NameServers"
+echo "[+] Iniciando tentativa de transferência de zona"
+for i in $(echo $NS)
+do
+  echo -e "\t[-] Testando NameServer $i do dominio $DOMAIN"
+  dig @$i -t axfr $DOMAIN +short
 done
-echo "[+] DONE"
-echo "[+] Cleaning"
-rm $DIR/$1.html
